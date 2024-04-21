d1230ab4173b3b4217e9c1e85ae624c2a0012f67
Otabek <otabek.islomjonov11@gmail.com>
2024-04-20 20:27:24 -0400

    navbar component added to product landing page

--- src/pages/ProductLanding/index.tsx
+++ src/pages/ProductLanding/index.tsx
@@ -6,6 +6,8 @@ import logo from '../../assets/logo.svg';
 import styles from './ProductLanding.module.scss';
 import { useAppContext } from '../../hooks/useAppContext';
 
+import Navbar from '../../components/Navbar'; // Import Navbar component
+
 function ProductLanding() {
   const { addItem, products, cartItems } = useAppContext();
 
@@ -24,6 +26,21 @@ function ProductLanding() {
       <h1 className={styles.title}>
         <img src={logo} alt="Daily deals" />
       </h1>
+     
+      <Navbar />
+
+     
+      {/* <div className="navbar-container">
+      <nav className="navbar">
+        <div className="navbar__link">Featured</div>
+        <div className="navbar__link">Jeans</div>
+        <div className="navbar__link">Pants</div>
+        <div className="navbar__link">Shorts</div>
+        <div className="navbar__link">Tops</div>
+        <div className="navbar__link">Outerwear</div>
+        <div className="navbar__link">Accessories</div>
+      </nav>
+    </div> */}
 
       <ProductList
         addItem={addItem}
